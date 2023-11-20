d53bbe1789d2c0c6bb403238f8fe2ee290b0cc54
Nuthapon Sripornprasert <nuthapon@swiftdynamics.co.th>
2023-11-20 08:51:22 +0700

    feat: set env

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	envMap, err := godotenv.Read()
+	if err != nil {
+		panic(err)
+	}
+
+	PORT := envMap["PORT"]
+
 	app := gin.New()
-	app.Run(":8000")
+	app.Run(":" + PORT)
 }

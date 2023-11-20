d688b57f75a269b9f8456cc9e6197d549e74e0eb
Nuthapon Sripornprasert <nuthapon@swiftdynamics.co.th>
2023-11-20 11:47:24 +0700

    fix: json name

--- models/blog.go
+++ models/blog.go
@@ -5,7 +5,7 @@ import "time"
 type Blog struct {
 	Id        uint   `json:"id" gorm:"primaryKey"`
 	Title     string `json:"title" gorm:"not null"`
-	Author    string `json:"title" default:"admin"`
+	Author    string `json:"author" default:"admin"`
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime:nano"`
 	UpdateAt time.Time `json:"updated_at" gorm:"autoUpdateTime:nano"`
 }

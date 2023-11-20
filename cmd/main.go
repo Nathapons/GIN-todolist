ed50379ec6c984e482ffce8a14caa9eab051e26d
Nuthapon Sripornprasert <nuthapon@swiftdynamics.co.th>
2023-11-20 11:05:38 +0700

    feat: swagger

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"main/router"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+
+	_ "main/docs"
 )
 
+// @title Blog todolist API
+// @version 1.0
+// @description Blog todolist API in Go using GIN framework
+// @host 	localhost:8000
+// @BasePath /api/v1
 func main() {
 	envMap, err := godotenv.Read()
 	if err != nil {
@@ -14,5 +23,8 @@ func main() {
 	PORT := envMap["PORT"]
 
 	app := gin.New()
+
+	router.SetUpRouter(app)
+
 	app.Run(":" + PORT)
 }
